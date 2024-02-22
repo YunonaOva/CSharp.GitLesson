886458f836666766d2892110689267c0dbede739
YunonaOva <odintsevayunona@yandex.ru>
2024-02-22 19:39:02 +0300

    Создала решение 20 б

--- CSharpEducation.Practice/task20/Program.cs
+++ CSharpEducation.Practice/task20/Program.cs
@@ -1,35 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-class program
+namespace Task20
 {
-// a) В методе Main создайте массив строк, содержащий имена студентов. Заполните его не менее чем 5-ю произвольными именами.
+    class program
+    {
+    // a) В методе Main создайте массив строк, содержащий имена студентов. Заполните его не менее чем 5-ю произвольными именами.
 
-public static void Main(string[] args)
+        public static void Main(string[] args)
+        {
+            string [] study = new string[5];     // создала массив
 
-{
-string [] study = new string[5];     // создала массив
+            for (int i = 0; i < study.Length; i++)
+            {
+                study[i] = Console.ReadLine();
+            }
 
-for (int i = 0; i < study.Length; i++)
-{
-    study[i] = Console.ReadLine();
-}
+            SpisokStudy(study);
+        }
 
-SpisokStudy();
-}
+        // b) Напишите метод для вывода на экран всех имён студентов из массива.
 
-// b) Напишите метод для вывода на экран всех имён студентов из массива.
+        public static void SpisokStudy(string [] spisok)
+        {
+            for (int i = 0; i < spisok.Length; i++)
+            {
+                Console.WriteLine(spisok[i]);
+            }
 
-public static void SpisokStudy()
-
-{
-    for (int i = 0; i < study.Length; i++)
-{
-         Console.WriteLine(study[i]);
-}
-
-}
-
-
-
-
+        }
+    }
 }
