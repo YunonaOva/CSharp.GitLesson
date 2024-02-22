e9845dd20e42d22bb007560c760b5f6d79bd1d99
YunonaOva <odintsevayunona@yandex.ru>
2024-02-22 19:05:43 +0300

    Создала решение 20 а

---
+++ CSharpEducation.Practice/task20/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class program
+{
+// a) В методе Main создайте массив строк, содержащий имена студентов. Заполните его не менее чем 5-ю произвольными именами.
+
+public static void Main(string[] args)
+{
+string [] study = new string[5];     // создала массив
+
+for (int i = 0; i < study.Length; i++)
+{
+    study[i] = Console.ReadLine();
+}
+for (int i = 0; i < study.Length; i++)
+{
+    Console.WriteLine(study[i]);
+}
+
+}
+}
