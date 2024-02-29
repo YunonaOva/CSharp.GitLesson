96207cf953125eeb296eb7313956ce9566f17507
YunonaOva <odintsevayunona@yandex.ru>
2024-02-29 19:59:45 +0300

    Создала решение по задаче 26

---
+++ CSharpEducation.Practice/Task21/Person.cs
@@ -0,0 +1,7 @@
+ class Person
+ {
+    public string name;                 // имя
+    public int age;                     // возраст
+
+
+ }
---
+++ CSharpEducation.Practice/Task21/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Formats.Asn1;
+
+Person tom = new Person();
+tom.name = "Tom";
+
+Person tom_age = new Person();
+tom_age.age = 18;
+
+Console.WriteLine(tom.name);
+Console.WriteLine(tom_age.age);
+
