854eea3a58d63e4dbd09d781c24d4731e0cf982b
YunonaOva <odintsevayunona@yandex.ru>
2024-03-04 22:04:58 +0300

    Создала решение по задаче 27

---
+++ CSharpEducation.Practice/Task22/Person.cs
@@ -0,0 +1,12 @@
+public class Person 
+{
+    public string name;                 // имя
+    public int age;                     // возраст
+
+public bool CheckIsAdult()
+    {
+        return age >= 18;
+    
+    }
+
+}
---
+++ CSharpEducation.Practice/Task22/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person();
+        person.age = 15;
+        person.name = "Дмитрий";
+
+        if (person.CheckIsAdult())
+        {
+            Console.WriteLine(person.age);
+            Console.WriteLine(person.name);
+        }
+        else
+        {
+            Console.WriteLine(person.name);
+        }
+       
+
+
+    }
+}
