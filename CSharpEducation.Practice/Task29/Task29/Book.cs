a0a7704a0ed1af2bc44cd87e907881905289033a
YunonaOva <odintsevayunona@yandex.ru>
2024-03-24 20:24:30 +0300

    Создала решение 29

---
+++ CSharpEducation.Practice/Task29/Task29/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task29
+{
+    public class Book             //Создайте в папке проекта файл Book.cs, и создайте в нём новый public класс Book. 
+    {
+        //* В класс добавьте поля, соответствующие названию книги, автору книги, год издания, жанр. 
+
+        public string Name_book;
+
+        public string Avtor;
+
+        public int Years;
+
+        public string Genre;
+
+
+        public static int BooksCount = 0;    // В класс добавьте static поле BooksCount, которое будет использоваться для подсчёта созданных книг. 
+
+        public Book (string name_book, string author, int year, string genre)         //Создайте конструктор класса, в котором увеличивайте значение BooksCount на единицу.
+        {
+        Name_book = name_book;
+        Avtor = author;
+        Years = year;
+        Genre = genre;   
+        
+        BooksCount++;
+        }
+
+
+
+    }
+}
+
---
+++ CSharpEducation.Practice/Task29/Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /** Создайте в папке проекта файл Book.cs, и создайте в нём новый public класс Book. 
+             * В класс добавьте static поле BooksCount, которое будет использоваться для подсчёта созданных книг. 
+             * Создайте конструктор класса, в котором увеличивайте значение BooksCount на единицу. 
+             * В класс добавьте поля, соответствующие названию книги, автору книги, год издания, жанр. 
+             * Создайте несколько книг в классе Program. Выведите на экран консоли значение BooksCount.*/
+
+            Book book1 = new Book(name_book: "Пиши, сокращай", author: "Максим Ильяхов", year: 2020, genre: "копирайтинг");
+
+            Book book2 = new Book(name_book: "Пикник на обочине", author: "братья Стругацкие", year: 1972, genre: "фантастика");
+
+            Console.WriteLine(Book.BooksCount);
+            Console.ReadLine();
+        }
+    }
+}
