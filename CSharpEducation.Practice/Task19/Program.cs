e0c72ba633e1bf215aea649220bf9120a9faafcf
YunonaOva <odintsevayunona@yandex.ru>
2024-03-24 11:58:53 +0300

    Создала решение 20

--- CSharpEducation.Practice/Task19/Program.cs
+++ CSharpEducation.Practice/Task19/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+/*
+ Создайте и реализуйте метод, который принимает на вход размер массива n и возвращает пустой массив указанного размера. 
+ Вызовите метод в методе Main и заполните его полученный массив. Содержимое массива выведите на экран.
+ */
+class Program
+{
+   public static void Main(string[] args)
+    {
+        int N = 5;
+
+        var Array = NewArray(N);
+
+        for (int i = 0; i < N; i++)
+        {
+            Array[i] = i;
+        }
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write(i);
+        }
+        Console.ReadLine();
+    }
+
+   public static int[] NewArray(int n)
+    {
+        return new int[n];
+    }
+}
