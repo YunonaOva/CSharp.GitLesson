8683c7401cee142ac3c97bcb7d246abb87bdbb06
YunonaOva <odintsevayunona@yandex.ru>
2024-03-24 13:40:38 +0300

    Создала решение 23

---
+++ CSharpEducation.Practice/Task19_23/Task19_23/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task19_23
+{
+    internal class Program
+    {
+        /*Создайте и реализуйте метод, который будет принимать два числа и менять их значения местами. 
+         * Вызовите метод в Main.*/
+
+        static void Main(string[] args)
+        {
+            int value1 = 5;
+            int value2 = 15;
+
+            Console.WriteLine("Элемент 1 до изменений: " + value1);
+            Console.WriteLine("Элемент 2 до изменений: " + value2);
+
+            ChangeElement(ref value1, ref value2);
+
+            Console.WriteLine("Элемент 1 после изменений: " + value1);
+            Console.WriteLine("Элемент 2 после изменений: " + value2);
+
+        }
+
+        static void ChangeElement(ref int value1, ref int value2)
+        { 
+         int temp = value1;
+         value1 = value2;
+         value2 = temp;
+
+        }
+    }
+}
