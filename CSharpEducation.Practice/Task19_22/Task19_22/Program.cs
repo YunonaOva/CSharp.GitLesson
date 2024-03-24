ae7d00de16f8badf16c1ecfa8fa0f67a01992777
YunonaOva <odintsevayunona@yandex.ru>
2024-03-24 13:27:33 +0300

    Само решение задачи 22

---
+++ CSharpEducation.Practice/Task19_22/Task19_22/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task19_22
+{
+    internal class Program
+
+
+        /*22. Создайте и реализуйте метод, который будет принимать число по ссылке (ref) и менять его знак на противоположный. 
+         * Метод должен принимать один аргумент и не возвращать ничего.*/
+    {
+        public static void Main(string[] args)
+        {
+            int elevent = 5;
+
+            Foo(ref elevent);
+
+            Console.WriteLine(elevent);
+
+            Console.ReadLine();
+        }
+
+        public static void Foo(ref int x)
+        { 
+            x *= -1; 
+        }
+
+    }
+}
