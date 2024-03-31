1565fed716b4677899ed4c2f804c47ca162334c5
YunonaOva <odintsevayunona@yandex.ru>
2024-03-31 12:21:34 +0300

    Задание по List (a,b)

---
+++ CSharpEducation.Practice/1 Задание по List/1 Задание по List/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _1_Задание_по_List
+{
+    internal class Program
+
+        // b) В Program.Main создайте список и студентов.
+
+    {
+        static void Main(string[] args)
+        {
+            List<Student> SpisokStudent = new List<Student>();
+
+            int p_menu;
+
+            do
+            {
+                Console.WriteLine("Меню: ");
+                Console.WriteLine("1. Добавить студента.");
+                Console.WriteLine("2. Удалить студента.");
+                Console.WriteLine("3. Вывести список студентов.");
+                Console.WriteLine("0. Выход.");
+                Console.WriteLine();
+                Console.Write("Введите пункт из меню: ");
+
+                p_menu = int.Parse(Console.ReadLine());
+
+                if (p_menu == 1)
+                {
+                    Console.Write("Введите имя студента: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Введите возраст студента: ");
+                    int age = int.Parse(Console.ReadLine());
+
+                    SpisokStudent.Add(new Student (name, age));
+
+                    Console.WriteLine();
+                }
+                else if (p_menu == 2)
+                {
+                    Console.WriteLine("Выбрали 2.");
+                    Console.WriteLine();
+                }
+                else if (p_menu == 3)
+                {
+                    if (SpisokStudent.Count == 0)
+                    {
+                        Console.WriteLine("Список студентов пуст.");
+                    }
+                    else
+                    {
+                        Print(SpisokStudent);
+                    }
+                    Console.WriteLine();
+                }
+                else
+                    Console.WriteLine("Введите еще раз пункт из меню.");
+                    Console.WriteLine();
+
+            }
+            while (p_menu != 0);
+
+          
+        }
+        private static void Print(List<Student> array)
+        {
+            foreach (var x in array)
+            {
+                Console.WriteLine($"Имя: {x.name}, Возраст: {x.age}");
+            }
+        }
+
+    }
+}
+
---
+++ CSharpEducation.Practice/1 Задание по List/1 Задание по List/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Задание_по_List
+
+    /* a) Создайте класс Student. 
+     * Добавьте в него два свойства Name и Age. 
+     * Свойства должны быть доступны только для чтения. 
+     * Инициализируйте их передачей значений в конструкторе.*/
+
+{
+     public class Student
+     {
+         public string name { get; }
+         public int age { get; }
+
+         public Student(string name, int age)
+         {
+             this.name = name;
+             this.age = age;
+         }
+
+     }
+}
